c8d1b047eb0248f3f8fa508dd40fec2fa68ddc05
Антон <@DESKTOP-C253HS0>
2022-11-07 07:33:54 +0300

    Arrays homework encoding

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -5,10 +5,39 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	const int SIZE = 5;
-	int arr[SIZE]={3,5,8};
-	arr[3] = 123;
+	int arr[SIZE];
+	//arr[3] = 123;
+	cout << "Введите значения элементов массива: ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}
+	
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << "\t";
 	}
+	cout << endl;
+	for (int i = SIZE-1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	int sum=0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива = " << sum << endl;
+	cout << "Сумма среднее-фрифметическое элементов массива: " << sum / SIZE << endl;
+
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+	cout << "Максимальное значение в массиве: " << max << endl;
 }
