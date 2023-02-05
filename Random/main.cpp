665443097df452d3b3135a5fbec969d4d28a6601
Антон <@DESKTOP-C253HS0>
2023-02-05 16:01:50 +0300

    kek

---
+++ Random/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+void main ()
+{
+	setlocale(LC_ALL, "ru");
+	const int n = 15;
+	int arr[n];
+	//rand();	//Функция rand() возвращает псевдослучайное число, в диапазоне от 0 до 32 767 (RAND_MAX)
+	//Заполнение массива случайными числами:
+	int minRand, maxRand;
+	do
+	{
+		cout << "Введите минимальное случайное число: "; cin >> minRand;
+		cout << "Введите максимальное случайное число: "; cin >> maxRand;
+		if (minRand >= maxRand)cout << "Error: значения некорректны, попробуйте еще раз" << endl;
+	} while (minRand >= maxRand);
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
